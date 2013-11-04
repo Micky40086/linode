f138a736fc5b394d051285832b2bcba86e0f5b91
Jin-Sih Lin <linpct@gmail.com>
2013-11-04 08:48:00 +0800

    sync

--- 02_nginx.sh
+++ 02_nginx.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+cd /lib/passenger
+sudo rake nginx
+cd ~/
+
 # 重編 nginx
 sudo abs
 mkdir src
-cp -rpf /tmp/abs/community/nginx src
+cp -rpf /var/abs/community/nginx src
 # @TODO 進入 nginx, 指定 --add-module=/lib/passenger/ext/nginx
