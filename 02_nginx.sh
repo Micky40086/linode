fd6b70ccce191b993d1c156eeb8775db5b2f6402
Jin-Sih Lin <linpct@gmail.com>
2013-11-04 23:12:24 +0800

    use raw instead of git clone

--- 02_nginx.sh
+++ 02_nginx.sh
@@ -11,4 +11,6 @@ cp -rpf /var/abs/community/nginx src
 # @TODO 進入 nginx, 指定 --add-module=/lib/passenger/ext/nginx
 
 # 複製 config
-sudo cp -rpf ./conf/nginx/* /etc/nginx/
+cd /etc/nginx
+sudo curl https://raw.github.com/pct/linode/master/conf/nginx/nginx.conf > nginx.conf
+sudo curl https://raw.github.com/pct/linode/master/conf/nginx/vhost.conf > vhost.conf
