5fd00500a44f26a9f05ba31a46d6bacb14926837
Jin-Sih Lin <linpct@gmail.com>
2015-06-10 13:42:26 +0800

    using pacaur -S aur/nginx-passenger

--- 02_nginx.sh
+++ 02_nginx.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
-# TODO 使用 root 身分
+# TODO 使用 user 身分
 
-cd /lib/passenger
-rake nginx
-cd ~/
-
-# 重編 nginx
-abs
-mkdir src
-cp -rpf /var/abs/extra/nginx src
-# @TODO 進入 nginx, 指定 --add-module=/lib/passenger/ext/nginx
-
-# 複製 config
+pacaur -S aur/nginx-passenger
 cd /etc/nginx
 mkdir logs
 curl https://raw.github.com/pct/linode/master/conf/nginx/nginx.conf > nginx.conf
 curl https://raw.github.com/pct/linode/master/conf/nginx/vhost.conf > vhost.conf
 
-echo 'Please make nginx'
+echo 'Please try nginx now!'
