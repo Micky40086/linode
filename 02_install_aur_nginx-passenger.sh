79da59f5b6de7e1744e6e5cf98862eeb14401582
Jin-Sih Lin <linpct@gmail.com>
2015-06-10 14:40:47 +0800

    add sudo to 02_install_aur_nginx-passenger.sh

--- 02_install_aur_nginx-passenger.sh
+++ 02_install_aur_nginx-passenger.sh
@@ -3,9 +3,10 @@
 # TODO 使用 user 身分
 
 pacaur -S aur/nginx-passenger
+
 cd /etc/nginx
-mkdir logs
-curl https://raw.github.com/pct/linode/master/conf/nginx/nginx.conf > nginx.conf
-curl https://raw.github.com/pct/linode/master/conf/nginx/vhost.conf > vhost.conf
+sudo mkdir logs
+sudo curl https://raw.github.com/pct/linode/master/conf/nginx/nginx.conf > nginx.conf
+sudo curl https://raw.github.com/pct/linode/master/conf/nginx/vhost.conf > vhost.conf
 
 echo 'Please see https://github.com/t-richards/aur-nginx-passenger#configuration and try nginx now!'
