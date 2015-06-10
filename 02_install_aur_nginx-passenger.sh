e0be8962bb03afa3a25e39b74f886338e871308a
Jin-Sih Lin <linpct@gmail.com>
2015-06-10 14:51:29 +0800

    sync 02_install_aur_nginx-passenger.sh

--- 02_install_aur_nginx-passenger.sh
+++ 02_install_aur_nginx-passenger.sh
@@ -8,5 +8,6 @@ cd /etc/nginx
 sudo mkdir -p logs
 sudo wget https://raw.github.com/pct/linode/master/conf/nginx/nginx.conf
 sudo wget https://raw.github.com/pct/linode/master/conf/nginx/vhost.conf
+sudo mv nginx.conf.1 nginx.conf
 
 echo 'Please see https://github.com/t-richards/aur-nginx-passenger#configuration and try nginx now!'
