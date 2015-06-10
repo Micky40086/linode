a96f1f5c69bb880b521aa4664f75e62881f75624
Jin-Sih Lin <linpct@gmail.com>
2015-06-10 14:14:43 +0800

    sync 03_imagemagick.sh

--- 03_imagemagick.sh
+++ 03_imagemagick.sh
@@ -5,7 +5,7 @@
 # 重編 imagemagick
 abs
 mkdir -p ~/src
-cp -rpf /var/abs/extra/imagemagick ~/src
+cp -rpf /var/abs/extra/imagemagick ~/src/
 # @TODO 進入 imagemagick, 去除 --enable-hdri (因為 rails carrierwave)
 
 echo 'Please make imagemagick'
