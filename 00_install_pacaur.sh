0f60a321b32c524e89d3dc046a419635f4445fe9
Jin-Sih Lin <linpct@gmail.com>
2015-08-28 04:11:19 +0000

    update 00_install_pacaur.sh

--- 00_install_pacaur.sh
+++ 00_install_pacaur.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
+export PATH=$PATH:/usr/bin/core_perl
+
 # @TODO 使用 user 身份
 
-gpg --list-keys
-gpg --recv-keys 1EB2638FF56C0C53 #cower
+#gpg --list-keys
+#gpg --recv-keys 1EB2638FF56C0C53 #cower
 
 mkdir -p ~/src
 cd ~/src
 
-wget https://aur.archlinux.org/packages/co/cower/cower.tar.gz
-wget https://aur.archlinux.org/packages/pa/pacaur/pacaur.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz
 
 tar xvf cower.tar.gz
 tar xvf pacaur.tar.gz
 
 cd cower
-makepkg -s
+makepkg -sc --skipinteg
 sudo pacman -U cower*.pkg.tar.xz
 cd -
 
 cd pacaur
-makepkg -s
+makepkg -sc --skipinteg
 sudo pacman -U pacaur*.pkg.tar.xz
 cd -
 
