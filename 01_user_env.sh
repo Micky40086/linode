dbf87aafd39b943fdfcbf3012a65774fb55088b4
Jin-Sih Lin <linpct@gmail.com>
2016-02-03 04:39:54 +0000

    zsh to fish

--- 01_user_env.sh
+++ 01_user_env.sh
@@ -2,15 +2,15 @@
 
 cd ~
 
-# 安裝 oh-my-zsh
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+# 安裝 oh-my-fish
+curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
 
 # 安裝 vimrc-core
 wget --no-check-certificate https://github.com/pct/vimrc-core/raw/master/install.sh -O - | sh
 vim +NeoBundleInstall +qall
 
 # 抓取 dot files
-cp -rp ./dotfiles/.* ~/ 
+cp -rp ./dotfiles/.* ~/
 
-# 變更為 zsh，path: /usr/bin/zsh
+# 變更為 fish，path: /usr/bin/fish
 chsh
