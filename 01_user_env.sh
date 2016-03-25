84c14683923cb3842e57f771d7307a264cb2bc3e
Jin-Sih Lin <linpct@gmail.com>
2016-03-26 01:39:08 +0800

    use vim +PlugInstall

--- 01_user_env.sh
+++ 01_user_env.sh
@@ -7,7 +7,7 @@ curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
 
 # 安裝 vimrc-core
 wget --no-check-certificate https://github.com/pct/vimrc-core/raw/master/install.sh -O - | sh
-vim +NeoBundleInstall +qall
+vim +PlugInstall +qall
 
 # 抓取 dot files
 cp -rp ./dotfiles/.* ~/
