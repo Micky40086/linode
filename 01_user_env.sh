7f7832be12fc33ea70efaf69615a0c83079d477c
Jin-Sih Lin <linpct@gmail.com>
2016-04-03 14:48:41 +0800

    add omf theme

--- 01_user_env.sh
+++ 01_user_env.sh
@@ -4,6 +4,8 @@ cd ~
 
 # 安裝 oh-my-fish
 curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
+# 使用 fox theme
+omf install fox
 
 # 安裝 vimrc-core
 wget --no-check-certificate https://github.com/pct/vimrc-core/raw/master/install.sh -O - | sh
