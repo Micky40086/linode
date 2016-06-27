2313a258c4ea2a781617968b41d78900c95efaa2
Jin-Sih Lin <linpct@gmail.com>
2016-06-27 22:57:52 +0800

    add conf/nginx.conf

--- 02_aur_nginx-mainline-passenger.sh
+++ 02_aur_nginx-mainline-passenger.sh
@@ -5,12 +5,10 @@
 pacaur -S aur/nginx-mainline-passenger
 
 cd /etc/nginx
-sudo mkdir -p logs
 sudo mkdir -p sites
-sudo wget https://raw.github.com/pct/linode/master/conf/nginx/vhost.conf
-sudo mv vhost.conf sites/main.conf
+sudo curl https://raw.github.com/pct/linode/master/conf/nginx/nginx.conf > nginx.conf
+sudo curl https://raw.github.com/pct/linode/master/conf/nginx/vhost.conf > sites/main.conf
 
 sudo systemctl enable nginx
 
-echo 'Please comment nginx _ sites and try now!'
 echo 'Please check if `systemctl enable nginx`'
