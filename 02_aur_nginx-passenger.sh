da3d4c05a05edbd5b9f933450b7506aa5c77ea76
Jin-Sih Lin <linpct@gmail.com>
2016-09-11 22:09:56 +0800

    change 02_aur_nginx-mainline-passenger.sh

--- 02_aur_nginx-passenger.sh
+++ 02_aur_nginx-passenger.sh
@@ -2,7 +2,8 @@
 
 # TODO 使用 user 身分
 
-pacaur -S aur/nginx-mainline-passenger
+pacaur -S aur/nginx-passenger
+#pacaur -S aur/nginx-mainline-passenger
 
 sudo mkdir -p /etc/nginx/sites
 sudo cp conf/nginx/nginx.conf /etc/nginx/
