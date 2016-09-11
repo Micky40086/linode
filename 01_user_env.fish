7e78d5e1e99035c7785e8aefedb69c05f6f87eb1
Jin-Sih Lin <linpct@gmail.com>
2016-09-11 20:41:05 +0800

    add rsync_tmbackup.sh

--- 01_user_env.fish
+++ 01_user_env.fish
@@ -18,3 +18,7 @@ echo '. ~/.config/fish/aliases.fish' >>  ~/.config/fish/config.fish
 mkdir -p ~/bin
 curl https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat > ~/bin/imgcat
 chmod +x ~/bin/imgcat
+
+# backup
+curl https://raw.githubusercontent.com/laurent22/rsync-time-backup/master/rsync_tmbackup.sh > ~/bin/rsync_tmbackup.sh
+chmod +x ~/bin/rsync_tmbackup.sh
