6a7d51880b401bafe9641f070149bbf4a246f4c7
Jin-Sih Lin <linpct@gmail.com>
2016-11-23 14:38:25 +0800

    fix user at tune

--- 00_tune_limit.sh
+++ 00_tune_limit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo 'DefaultLimitNOFILE=65535' >> /etc/systemd/system.conf
 
-echo "soft nofile 4096\nhard nofile 4096" >> /etc/security/limits.conf
+echo "http soft nofile 4096\nhttp hard nofile 4096" >> /etc/security/limits.conf
