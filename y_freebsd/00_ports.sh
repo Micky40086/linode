c7707d8b1e79cd957682c24bff0175821b73ef88
Jin-Sih Lin <linpct@gmail.com>
2016-11-30 18:47:20 +0800

    add mothra

--- y_freebsd/00_ports.sh
+++ y_freebsd/00_ports.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 # READ https://github.com/pct/freebsd_utils
-pkg install portmaster subversion genplist porttools py27-lxml autoconf m4
+pkg install portmaster subversion genplist porttools py27-lxml autoconf m4 ruby22-gems
 svn checkout https://svn.FreeBSD.org/ports/head /usr/ports
+
+gem install mothra
