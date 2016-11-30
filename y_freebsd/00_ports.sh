aaa74b0702807900e006d84ad6149e41ec4fd02a
Jin-Sih Lin <linpct@gmail.com>
2016-11-30 18:35:21 +0800

    sync 00_ports.sh

--- y_freebsd/00_ports.sh
+++ y_freebsd/00_ports.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # READ https://github.com/pct/freebsd_utils
-pkg install portmaster subversion
+pkg install portmaster subversion genplist porttools py27-lxml autoconf m4
 svn checkout https://svn.FreeBSD.org/ports/head /usr/ports
