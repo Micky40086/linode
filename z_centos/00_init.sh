d7d732c7f9c8aa166b5db1205ad379d77328ef2b
root <root@vp2.mysbir.top>
2016-12-10 03:48:31 -0500

    add apache + passenger for centos

--- z_centos/00_init.sh
+++ z_centos/00_init.sh
@@ -2,7 +2,8 @@
 
 # root 身份
 yum install -y yum-utils 
-yum install -y curl git tmux vim
+yum install -y curl git tmux vim sudo
 yum install -y epel-release
+yum-config-manager --enable epel
 yum upgrade
 yum install -y nodejs
--- z_centos/01_apache_passenger.sh
+++ z_centos/01_apache_passenger.sh
@@ -4,8 +4,5 @@ yum install -y pygpgme curl
 
 curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
 
-yum install -y nginx passenger
+yum install -y mod_passenger
 
-echo ""
-
-echo 'set passenger at /etc/nginx/conf.d/passenger.conf'
