db2b77ca806dfae20b4a5f02ac135eb22e016bfb
Jin-Sih Lin <linpct@gmail.com>
2016-12-17 08:34:13 +0800

    sync todo_03

---
+++ todo_03_nginx_ssl.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#root 執行
+
+EMAIL='xxx@example.com'
+WEBROOT='/srv/http/xxx/public'
+DOMAIN='xxx.xxx.xxx'
+
+pacman -S certbot certbot-nginx
+
+certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN
+
+echo "1 1 1 * * root certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN" >> /etc/crontab
