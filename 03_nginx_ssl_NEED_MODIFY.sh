fc337e626e73fa1dd92c80a194bcd43748101b68
Jin-Sih Lin <linpct@gmail.com>
2017-03-23 11:11:05 +0800

    set multi domain at 03_nginx_ssl_NEED_MODIFY.sh

--- 03_nginx_ssl_NEED_MODIFY.sh
+++ 03_nginx_ssl_NEED_MODIFY.sh
@@ -8,6 +8,6 @@ DOMAIN='xxx.xxx.xxx'
 
 pacman -Sy certbot certbot-nginx
 
-certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN
+certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN # -d DOMAIN2 -d DOMAIN3
 
 echo "1 1 1 * * root certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN" >> /etc/crontab
