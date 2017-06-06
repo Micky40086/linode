8eab9f799aa17b20af36bce698dadfc1a9dd1a52
Jin-Sih Lin <linpct@gmail.com>
2017-06-06 11:50:47 +0800

    use cerbot renew

--- todo_03_nginx_ssl.sh
+++ todo_03_nginx_ssl.sh
@@ -10,4 +10,5 @@ pacman -S certbot certbot-nginx
 
 certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN
 
-echo "1 1 1 * * root certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN" >> /etc/crontab
+#echo "1 1 1 * * root certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN" >> /etc/crontab
+echo "1 1 1 * * root certbot renew" >> /etc/crontab
