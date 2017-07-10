61f7ef53d1fce82d80f96a5ba221f845ad0db4be
Jin-Sih Lin <linpct@gmail.com>
2017-07-10 13:19:26 +0800

    FIX: 00_tune_limit.sh

--- 00_tune_limit.sh
+++ 00_tune_limit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo 'DefaultLimitNOFILE=65535' >> /etc/systemd/system.conf
 
-echo "http soft nofile 4096\nhttp hard nofile 4096" >> /etc/security/limits.conf
+echo -e "http soft nofile 4096\nhttp hard nofile 4096" >> /etc/security/limits.conf
