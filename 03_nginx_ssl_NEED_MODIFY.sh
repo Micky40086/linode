c865a367ebe850608adfbe1769e80e917af26bbc
Jin-Sih Lin <linpct@gmail.com>
2017-07-24 11:39:12 +0800

    check ssl each week

--- 03_nginx_ssl_NEED_MODIFY.sh
+++ 03_nginx_ssl_NEED_MODIFY.sh
@@ -10,5 +10,5 @@ pacman -Sy certbot certbot-nginx
 
 certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN # -d DOMAIN2 -d DOMAIN3
 
-#echo "1 1 1 * * root certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN" >> /etc/crontab
-echo "1 1 1 * * root certbot renew" >> /etc/crontab
+#echo "1 1 * * * root certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN" >> /etc/crontab
+echo "1 1 * * * root certbot renew" >> /etc/crontab
