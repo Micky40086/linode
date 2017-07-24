693a3e2de549fd38c2216732b3e22ab1c6eb9682
Jin-Sih Lin <linpct@gmail.com>
2017-07-24 07:46:26 +0000

    add ruby24 for centos

--- z_centos7/02_ruby24.sh
+++ z_centos7/02_ruby24.sh
@@ -4,7 +4,7 @@ gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB
 curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm requirements
-rvm install 2.3.0
-rvm use 2.3.0 --default
-gem install rails bundler passenger
+rvm install 2.4.1
+rvm use 2.4.1 --default
+gem install bundler 
 
