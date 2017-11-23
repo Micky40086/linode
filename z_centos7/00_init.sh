764256b3682daae8221e023f2faef89f2b9356c8
Jin-Sih Lin <linpct@gmail.com>
2017-11-23 21:47:24 +0800

    sync z_centos7/00_init.sh

--- z_centos7/00_init.sh
+++ z_centos7/00_init.sh
@@ -3,14 +3,13 @@
 # root 身份
 yum install -y yum-plugin-fastestmirror.noarch
 yum install -y yum-utils 
-yum install -y curl git tmux vim sudo net-tools wget fish tig lbzip2 axel sqlite sqlite-devel 
+yum install -y curl tmux vim sudo net-tools wget fish tig lbzip2 axel sqlite sqlite-devel libcurl-devel
 yum install -y epel-release
 yum install -y ImageMagick-devel GraphicsMagick-devel
 yum-config-manager --enable epel
 yum upgrade
 yum install -y nodejs
 
-yum remove git
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm
 yum install -y git2u
 
