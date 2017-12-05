af9e7aa7de7c1fb521c2b0ca9f403f91f251c5e3
Jin-Sih Lin <linpct@gmail.com>
2017-12-05 19:02:52 +0800

    add 02_nginx-mod-passenger.sh

---
+++ 02_nginx-mod-passenger.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+pacaur -S nginx-mod-passenger
+
+sudo mkdir -p /etc/nginx/sites
+sudo cp conf/nginx/nginx.conf /etc/nginx/
+sudo cp conf/nginx/vhost.conf /etc/nginx/sites/main.conf
+
+sudo systemctl enable nginx
+
+echo 'Please check if `systemctl enable nginx`'
