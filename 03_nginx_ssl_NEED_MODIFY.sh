dd8d081d3abed8cab11a7fa15a114b5249e4b89a
Jin-Sih Lin <linpct@gmail.com>
2017-12-12 20:36:51 +0800

    sync

--- 03_nginx_ssl_NEED_MODIFY.sh
+++ 03_nginx_ssl_NEED_MODIFY.sh
@@ -12,4 +12,4 @@ certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN # -d DOMAIN2 -d
 
 #echo "1 1 * * * root certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN" >> /etc/crontab
 # each Monday
-echo "1 1 * * 1 root certbot renew" >> /etc/crontab
+echo "1 1 * * 1 root certbot renew; systemctl reload nginx" >> /etc/crontab
