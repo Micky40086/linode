1020e33c567358b00da5d8dbe77ad4215a7aa795
Jin-Sih Lin <linpct@gmail.com>
2018-01-17 11:00:32 +0800

    add bin/compress_images.sh

---
+++ bin/compress_images.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+find ./* -type f -name "*.jpeg" -exec jpegoptim -s {} \;
+find ./* -type f -name "*.jpg" -exec jpegoptim -s {} \;
+find ./* -type f -name "*.png" -exec optipng -strip all {} \;
