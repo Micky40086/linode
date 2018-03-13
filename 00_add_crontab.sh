4156c37d4a3a126a22adf80751add4c9d8e2b6ec
Jin-Sih Lin <linpct@gmail.com>
2018-03-13 18:20:17 +0900

    sync bin/gen_sitemap.sh

--- 00_add_crontab.sh
+++ 00_add_crontab.sh
@@ -3,10 +3,18 @@
 
 echo 'user:ROOT'
 
-# backup
 BIN_PATH='/home/pct/bin'
 mkdir -p $BIN_PATH
+
+# backup
 cp ./backup/db_backup.sh $BIN_PATH
 chmod +x $BIN_PATH/db_backup.sh
+
+# sitemap
+cp ./bin/gen_sitemap.sh $BIN_PATH
+chmod +x $BIN_PATH/gen_sitemap.sh
+
 chown -R pct $BIN_PATH
+
+echo '12 1 * * * pct /home/pct/bin/gen_sitemap.sh' >> /etc/crontab
 echo '12 2 * * * pct /home/pct/bin/db_backup.sh' >> /etc/crontab
