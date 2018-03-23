07dca63d34477b3e328cc259ea9f68bb274ca39e
Jin-Sih Lin <linpct@gmail.com>
2018-03-23 10:57:43 +0900

    fix typo

--- 02_nginx_puma.sh
+++ 02_nginx_puma.sh
@@ -9,7 +9,7 @@ sudo cp conf/nginx/vhost.conf /etc/nginx/sites/main.conf
 sudo systemctl enable nginx
 
 sudo cp ./bin/check_puma.rb /home/pct/bin
-echo '*/5 1 * * * root /home/pct/bin/check_puma.rb' | sudo tee --append /etc/crontab
+echo '*/5 * * * * root /home/pct/bin/check_puma.rb' | sudo tee --append /etc/crontab
 
 echo 'Please check /etc/nginx/sites/main.conf'
 echo 'Please check /home/pct/bin/check_puma.rb'
