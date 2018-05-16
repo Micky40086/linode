b714e2036f49573cdb645e563290047bc246dc26
Jin-Sih Lin <linpct@gmail.com>
2018-05-16 13:26:13 +0800

    update check_puma.rb

--- bin/check_puma.rb
+++ bin/check_puma.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-CHECK_CMD = 'pgrep puma'
+CHECK_CMD = "ps ax|grep 'puma: cluster worker' |grep -v auto"
 RUN_PUMA = 'cd /srv/http/xxx; puma -C config/puma_production.rb'
 
 ret = system CHECK_CMD
