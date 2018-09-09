b8503ac7c5f4e4b78bf9945cb334bc37f59b5af2
Jin-Sih Lin <linpct@gmail.com>
2018-09-09 23:49:22 +0800

    add www to 03_nginx_ssl_NEED_MODIFY.sh

--- 03_nginx_ssl_NEED_MODIFY.sh
+++ 03_nginx_ssl_NEED_MODIFY.sh
@@ -5,10 +5,11 @@
 EMAIL='xxx@example.com'
 WEBROOT='/srv/http/xxx/public'
 DOMAIN='xxx.xxx.xxx'
+DOMAIN2='www.xxx.xxx.xxx'
 
 pacman -Sy certbot certbot-nginx
 
-certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN # -d DOMAIN2 -d DOMAIN3
+certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN -d $DOMAIN2 #-d DOMAIN3
 
 #echo "1 1 * * * root certbot certonly --email $EMAIL --webroot -w $WEBROOT -d $DOMAIN" >> /etc/crontab
 # each Monday
