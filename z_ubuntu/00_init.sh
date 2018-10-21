2fa785c91116fe954ec4631bf05db2982275e7ec
Jin-Sih Lin <linpct@gmail.com>
2018-10-21 16:13:27 +0800

    add locales to ubuntu init

--- z_ubuntu/00_init.sh
+++ z_ubuntu/00_init.sh
@@ -6,7 +6,7 @@
 apt update; apt dist-upgrade
 
 # 安裝相關套件
-apt install -y imagemagick nodejs ruby ruby-dev git tig vim fish curl tmux nginx net-tools
+apt install -y imagemagick nodejs ruby ruby-dev git tig vim fish curl tmux nginx net-tools locales
 
 apt install -y mariadb-client mariadb-server libmariadb-client-lgpl-dev-compat libmariadb-client-lgpl-dev libnetcdf-dev libssl-dev libcrypto++-dev libsqlite3-dev sqlite3
 
