e38e21a707b48d2d9df32e0acec390572067edc2
Jin-Sih Lin <linpct@gmail.com>
2019-03-11 20:59:23 +0800

    add nvim to mac

--- z_mac/00_init.sh
+++ z_mac/00_init.sh
@@ -4,5 +4,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # 安裝套件
-brew install ruby mariadb vim git wget tmux node tcptraceroute imagemagick fish hub tig axel sqlite tree ssh-copy-id unrar yarn
+brew install ruby mariadb vim git wget tmux node tcptraceroute imagemagick fish hub tig axel sqlite tree ssh-copy-id unrar yarn nvim
 
