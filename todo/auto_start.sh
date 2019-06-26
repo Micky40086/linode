8164c3f888cfc435547fe38fdc202673e241775f
Jin-Sih Lin <linpct@gmail.com>
2019-06-26 18:01:22 +0800

    sync todo/auto_start.sh

--- todo/auto_start.sh
+++ todo/auto_start.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
 # https://ginqi7.github.io/posts/archlinux-autostarting.html
 
 # 放到  /root 且要有執行權限
+PROJECT_NAME=xxx
+
+cd /srv/http/$PROJECT_NAME
+export RAILS_ENV=production
+unset SSH_ASKPASS
+puma -C ./config/puma_production.rb
+
+# reload nginx
+sudo systemctl reload nginx
